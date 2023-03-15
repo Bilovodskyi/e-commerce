0f94c9c66e8e761404ae95d92fff8ed20d923789
Bogdan Bilovodskyi <bogdanbilovodskyi@MacBook-Air-Bogdan.local>
2023-03-15 14:37:13 -0700

    bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -4,23 +4,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { theme, StyledSnack } from "./utils/theme";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store";
 import { SnackbarProvider } from "notistack";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <Provider store={store}>
-            <ThemeProvider theme={theme}>
-                <SnackbarProvider
-                    maxSnack={3}
-                    Components={{
-                        success: StyledSnack,
-                        error: StyledSnack,
-                        info: StyledSnack,
-                    }}>
-                    <App />
-                </SnackbarProvider>
-            </ThemeProvider>
+            <PersistGate loading={null} persistor={persistor}>
+                <ThemeProvider theme={theme}>
+                    <SnackbarProvider
+                        maxSnack={3}
+                        Components={{
+                            success: StyledSnack,
+                            error: StyledSnack,
+                            info: StyledSnack,
+                        }}>
+                        <App />
+                    </SnackbarProvider>
+                </ThemeProvider>
+            </PersistGate>
         </Provider>
     </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,48 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./cartSlice";
-import wishlistReducer from "./wishlistSlice";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import cartSlice from "./cartSlice";
+import wishlistSlice from "./wishlistSlice";
 import currencySlice from "./currencySlice";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+};
+
+const rootReducer = combineReducers({
+    cart: cartSlice,
+    wishlist: wishlistSlice,
+    currency: currencySlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-        wishlist: wishlistReducer,
-        currency: currencySlice,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
 });
+
+export let persistor = persistStore(store);
